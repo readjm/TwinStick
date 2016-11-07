df68dae848613487cb81685fdbf08634eca714b0
James Read <jread82@gmail.com>
2016-11-07 13:53:08 -0500

    Updated Test.cs

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,6 +6,7 @@ public class Test : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("Hello World");
+        Debug.Log("I am a Robot");
 	}
 	
 	// Update is called once per frame
